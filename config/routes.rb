14d60f05984c91bf29dd47f350f046ef288c1bd9
zafinar <zafinar@gmail.com>
2018-03-19 16:23:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'show', to: 'static#show'
+  resources :coupons, only: [:post, :get, :create, :index, :show, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
